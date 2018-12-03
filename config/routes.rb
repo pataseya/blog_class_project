1eb653145b8e3a64e24817e256a13fcc411ad1ec
PAT CAMP <PATS0825@MSN.COM>
2018-12-03 08:35:13 -0500

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts
-  get 'index' => 'practice#index'
+  get 'index' => 'practice/index'
   get 'about' =>'practice#about'
   post 'index' => 'practice#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+# here are the routes you get when you generate a model or
+# scaffold for a resource
+
+  #get "/posts" => "posts#index", as: :posts
+  #
+  # get "/posts/new" => "posts#new", as: :new_post
+  #
+  #get "/post/:id" => "posts#edit", as: :edit_post
+  #
+  #post "/posts" => "posts#create"
+  #
+  #patch "/posts" => "posts#update"
+  #
+  #delete "/post/:id" => "posts#destroy", as: :destroy_post
+
 end
